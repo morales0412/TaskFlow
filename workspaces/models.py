98b40aaa8c752e9bc04b7229fd1cc8a9d296b8dd
morales04 <andres.4moscoso@gmail.com>
2026-08-03 09:57:36 -0500

    creacion modelo workspaces

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.auth.models import User
+
 
 # Create your models here.
+class Workspace(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="workspaces")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["name", "owner"], name="unique_workspace_name_per_user"
+            )
+        ]
+
+    def __str__(self):
+        return self.name
