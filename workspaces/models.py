ed584b6c3c7c611e56716f33423f2d860bca0484
morales04 <andres.4moscoso@gmail.com>
2026-08-06 10:49:21 -0500

    creacion modelo usuario personalizado y reinicio de migraciones

--- workspaces/models.py
+++ workspaces/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 # Create your models here.
@@ -7,7 +7,9 @@ class Workspace(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="workspaces")
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="workspaces"
+    )
 
     class Meta:
         constraints = [
